bfdf0d4aaeb639dbddfe7bc728aa2442f0f97420
Felipe <fb.dc.col@gmail.com>
2019-11-11 19:53:29 -0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(stringNames, event){
+    let thankYouCards = []
+    for (let i = 0; i < stringNames.length; i++) {
+        thankYouCards.push(`Thank you, ${stringNames[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYouCards
+}
+
+function countDown(number){
+    while (number >= 0) {
+        console.log(number)
+        number--
+    }
+}
