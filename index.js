1f30c5faf6aabfcac7d92815406d5e8bbc0fe663
Kelley Chaplain <kmchaplain@gmail.com>
2019-11-12 20:32:09 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(arrayName, eventName) {
+  let thankYouCards = []
+  for ( let i = 0; i < arrayName.length; i++ ) {
+    thankYouCards.push(`Thank you, ${arrayName[i]}, for the wonderful ${eventName} gift!` );
+  }
+  return thankYouCards;
+}
+
+function countDown( startingNum ) {
+  while ( startingNum > 0 ) {
+    console.log( startingNum );
+    startingNum -= 1;
+  }
+  console.log( startingNum );
+}
