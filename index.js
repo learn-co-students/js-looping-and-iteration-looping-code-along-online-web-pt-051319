d15d474f2a10facf7cf6316392bb8ee109ccd4a2
Kristin Kraemer <kristinhannahk@gmail.com>
2019-11-15 00:58:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solutions in this file
+  const cards = []
+function writeCards(array, eventName) {
+  for(let i = 0; i < array.length; i++){
+   cards.push(`Thank you, ${array[i]}, for the wonderful ${eventName} gift!`)
+  }
+  return cards 
+}
