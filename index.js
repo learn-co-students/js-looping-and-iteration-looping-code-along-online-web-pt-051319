3db8e7b82c62beb395db0161b5ad0e8817a1cff2
lexus2727 <lexus2424@yahoo.com>
2019-11-25 19:52:11 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+
+function writeCards(cards, event) {
+   let newArray = [];
+    for (let i = 0; i < cards.length; i++) {
+        newArray.push( `Thank you, ${cards[i]}, for the wonderful ${event} gift!`)
+    }
+    return newArray;
+}
+
+
+
+function countDown(n) {
+
+
+while (n >= 0) {
+  console.log(n--);
+ }
+}
