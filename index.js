4aa573db6a7bc0477b88ca2ec7e5e41725e722a3
Michael Mottola <mottola64@gmail.com>
2020-01-08 12:57:03 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(arrayName, eventName) {
+    let thankYouCards = []
+    for (let i = 0; i < arrayName.length; i++){
+        thankYouCards.push(`Thank you, ${arrayName[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return thankYouCards;
+}
+
+function countDown(number){
+    while (number>0){
+        console.log(number);
+        number-=1;
+    }
+    console.log(number);
+}
